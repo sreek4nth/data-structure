fb0426b5bafe4074fed7518fc89ebd22f918c055
sreek4nth <sreekanthpradeep2002@gmail.com>
2023-11-27 16:23:11 +0530

    test

--- cycle3/rdb.c
+++ cycle3/rdb.c
@@ -1,376 +1,344 @@
-#include<stdio.h>
-#include<malloc.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct node 
-{
-    int n;
-    int degree;
-    struct node* parent;
-    struct node* child;
-    struct node* sibling;
+// Node structure for Red-Black Tree
+struct Node {
+    int data;
+    char color; // 'R' for red, 'B' for black
+    struct Node *left, *right, *parent;
 };
 
-struct node* MAKE_bin_HEAP();
-int bin_LINK(struct node*, struct node*);
-struct node* CREATE_NODE(int);
-struct node* bin_HEAP_UNION(struct node*, struct node*);
-struct node* bin_HEAP_INSERT(struct node*, struct node*);
-struct node* bin_HEAP_MERGE(struct node*, struct node*);
-struct node* bin_HEAP_EXTRACT_MIN(struct node*);
-int REVERT_LIST(struct node*);
-int DISPLAY(struct node*);
-struct node* FIND_NODE(struct node*, int);
-int bin_HEAP_DECREASE_KEY(struct node*, int, int);
-int bin_HEAP_DELETE(struct node*, int);
-int count = 1;
-
-struct node* MAKE_bin_HEAP() 
-{
-    struct node* np;
-    np = NULL;
-    return np;
+struct Node *createNode(int data) {
+    struct Node *newNode = (struct Node *)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->color = 'R'; // New nodes are always red
+    newNode->left = newNode->right = newNode->parent = NULL;
+    return newNode;
 }
 
-struct node * H = NULL;
-struct node *Hr = NULL;
- 
-int bin_LINK(struct node* y, struct node* z) 
-{
-    y->parent = z;
-    y->sibling = z->child;
-    z->child = y;
-    z->degree = z->degree + 1;
+// Function prototypes
+void leftRotate(struct Node **root, struct Node *x);
+void rightRotate(struct Node **root, struct Node *y);
+void insertFixup(struct Node **root, struct Node *z);
+void insertNode(struct Node **root, int data);
+void transplant(struct Node **root, struct Node *u, struct Node *v);
+struct Node *treeMinimum(struct Node *x);
+void deleteFixup(struct Node **root, struct Node *x);
+void deleteNode(struct Node **root, int data);
+void inOrderTraversal(struct Node *root);
+
+int main() {
+    struct Node *root = NULL;
+    int choice, data;
+
+    do {
+        printf("\n------ Red-Black Tree Operations ------\n");
+        printf("1. Insert\n");
+        printf("2. Delete\n");
+        printf("3. Display (In-Order Traversal)\n");
+        printf("4. Quit\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+
+        switch (choice) {
+            case 1:
+                printf("Enter data to insert: ");
+                scanf("%d", &data);
+                insertNode(&root, data);
+                printf("Data %d inserted successfully.\n", data);
+                break;
+
+            case 2:
+                printf("Enter data to delete: ");
+                scanf("%d", &data);
+                deleteNode(&root, data);
+                printf("Data %d deleted successfully.\n", data);
+                break;
+
+            case 3:
+                printf("In-Order Traversal:\n");
+                inOrderTraversal(root);
+                printf("\n");
+                break;
+
+            case 4:
+                printf("Quitting the program.\n");
+                break;
+
+            default:
+                printf("Invalid choice. Please enter a valid option.\n");
+        }
+
+    } while (choice != 4);
+
+    return 0;
 }
 
-struct node* CREATE_NODE(int k) 
-{
-    struct node* p;//new node;
-    p = (struct node*) malloc(sizeof(struct node));
-    p->n = k;
-    return p;
+// Left rotation in Red-Black Tree
+void leftRotate(struct Node **root, struct Node *x) {
+    struct Node *y = x->right;
+    x->right = y->left;
+   
+    if (y->left != NULL)
+        y->left->parent = x;
+
+    y->parent = x->parent;
+
+    if (x->parent == NULL)
+        *root = y;
+    else if (x == x->parent->left)
+        x->parent->left = y;
+    else
+        x->parent->right = y;
+
+    y->left = x;
+    x->parent = y;
 }
 
-struct node* bin_HEAP_UNION(struct node* H1, struct node* H2) 
-{
-    struct node* prev_x;
-    struct node* next_x;
-    struct node* x;
-    struct node* H = MAKE_bin_HEAP();
-    H = bin_HEAP_MERGE(H1, H2);
-    if (H == NULL)
-        return H;
-    prev_x = NULL;
-    x = H;
-    next_x = x->sibling;
-    while (next_x != NULL) 
-    {
-        if ((x->degree != next_x->degree) || ((next_x->sibling != NULL)
-                && (next_x->sibling)->degree == x->degree)) 
-                {
-            prev_x = x;
-            x = next_x;
-        	} 
-        else 
-        {
-            if (x->n <= next_x->n) 
-            {
-                x->sibling = next_x->sibling;
-                bin_LINK(next_x, x);
-            } 
-            else 
-            {
-                if (prev_x == NULL)
-                    H = next_x;
-                else
-                    prev_x->sibling = next_x;
-                bin_LINK(x, next_x);
-                x = next_x;
+// Right rotation in Red-Black Tree
+void rightRotate(struct Node **root, struct Node *y) {
+    struct Node *x = y->left;
+    y->left = x->right;
+
+    if (x->right != NULL)
+        x->right->parent = y;
+
+    x->parent = y->parent;
+
+    if (y->parent == NULL)
+        *root = x;
+    else if (y == y->parent->left)
+        y->parent->left = x;
+    else
+        y->parent->right = x;
+
+    x->right = y;
+    y->parent = x;
+}
+
+// Fix the Red-Black Tree properties after insertion
+void insertFixup(struct Node **root, struct Node *z) {
+    while (z->parent != NULL && z->parent->color == 'R') {
+        if (z->parent == z->parent->parent->left) {
+            struct Node *y = z->parent->parent->right;
+           
+            if (y != NULL && y->color == 'R') {
+                z->parent->color = 'B';
+                y->color = 'B';
+                z->parent->parent->color = 'R';
+                z = z->parent->parent;
+            } else {
+                if (z == z->parent->right) {
+                    z = z->parent;
+                    leftRotate(root, z);
+                }
+
+                z->parent->color = 'B';
+                z->parent->parent->color = 'R';
+                rightRotate(root, z->parent->parent);
+            }
+        } else {
+            struct Node *y = z->parent->parent->left;
+           
+            if (y != NULL && y->color == 'R') {
+                z->parent->color = 'B';
+                y->color = 'B';
+                z->parent->parent->color = 'R';
+                z = z->parent->parent;
+            } else {
+                if (z == z->parent->left) {
+                    z = z->parent;
+                    rightRotate(root, z);
+                }
+
+                z->parent->color = 'B';
+                z->parent->parent->color = 'R';
+                leftRotate(root, z->parent->parent);
             }
         }
-        next_x = x->sibling;
     }
-    return H;
-}
- 
-struct node* bin_HEAP_INSERT(struct node* H, struct node* x) 
-{
-    struct node* H1 = MAKE_bin_HEAP();
-    x->parent = NULL;
-    x->child = NULL;
-    x->sibling = NULL;
-    x->degree = 0;
-    H1 = x;
-    H = bin_HEAP_UNION(H, H1);
-    return H;
+
+    (*root)->color = 'B'; // Root should always be black
 }
 
-struct node* bin_HEAP_MERGE(struct node* H1, struct node* H2) 
-{
-    struct node* H = MAKE_bin_HEAP();
-    struct node* y;
-    struct node* z;
-    struct node* a;
-    struct node* b;
-    y = H1;
-    z = H2;
-    if (y != NULL) 
-    {
-        if (z != NULL && y->degree <= z->degree)
-            H = y;
-        else if (z != NULL && y->degree > z->degree)
-            /* need some modifications here;the first and the else conditions
-            can be merged together!!!! */
-            H = z;
+// Insert a node into the Red-Black Tree
+void insertNode(struct Node **root, int data) {
+    struct Node *z = createNode(data);
+    struct Node *y = NULL;
+    struct Node *x = *root;
+
+    while (x != NULL) {
+        y = x;
+        if (z->data < x->data)
+            x = x->left;
         else
-            H = y;
-    } else
-        H = z;
-    while (y != NULL && z != NULL) 
-    {
-        if (y->degree < z->degree) 
-        {
-            y = y->sibling;
-        } 
-        else if (y->degree == z->degree) 
-        {
-            a = y->sibling;
-            y->sibling = z;
-            y = a;
-        } else 
-        {
-            b = z->sibling;
-            z->sibling = y;
-            z = b;
-        }
+            x = x->right;
     }
-    return H;
-}
 
-int DISPLAY(struct node* H) 
-{
-    struct node* p;
-    if (H == NULL) 
-    {
-        printf("\nHEAP EMPTY");
-        return 0;
-    }
-    printf("\nTHE ROOT NODES ARE:-\n");
-    p = H;
-    while (p != NULL) 
-    {
-        printf("%d", p->n);
-        if (p->sibling != NULL)
-            printf("-->");
-        p = p->sibling;
-    }
-    printf("\n");
-}
+    z->parent = y;
 
-struct node* bin_HEAP_EXTRACT_MIN(struct node* H1) 
-{
-    int min;
-    struct node* t = NULL;
-    struct node* x = H1;
-    struct node *Hr;
-    struct node* p;
-    Hr = NULL;
-    if (x == NULL) 
-    {
-        printf("\nNOTHING TO EXTRACT");
-        return x;
-    }
-    //    int min=x->n;
-    p = x;
-    while (p->sibling != NULL) 
-    {
-        if ((p->sibling)->n < min) 
-        {
-            min = (p->sibling)->n;
-            t = p;
-            x = p->sibling;
-        }
-        p = p->sibling;
-    }
-    if (t == NULL && x->sibling == NULL)
-        H1 = NULL;
-    else if (t == NULL)
-        H1 = x->sibling;
-    else if (t->sibling == NULL)
-        t = NULL;
+    if (y == NULL)
+        *root = z;
+    else if (z->data < y->data)
+        y->left = z;
     else
-        t->sibling = x->sibling;
-    if (x->child != NULL) 
-    {
-        REVERT_LIST(x->child);
-        (x->child)->sibling = NULL;
-    }
-    H = bin_HEAP_UNION(H1, Hr);
+        y->right = z;
+
+    insertFixup(root, z);
+}
+
+// Transplant a subtree in the Red-Black Tree
+void transplant(struct Node **root, struct Node *u, struct Node *v) {
+    if (u->parent == NULL)
+        *root = v;
+    else if (u == u->parent->left)
+        u->parent->left = v;
+    else
+        u->parent->right = v;
+
+    if (v != NULL)
+        v->parent = u->parent;
+}
+
+// Find the minimum node in a Red-Black Tree
+struct Node *treeMinimum(struct Node *x) {
+    while (x->left != NULL)
+        x = x->left;
     return x;
 }
- 
-int REVERT_LIST(struct node* y) 
-{
-    if (y->sibling != NULL) 
-    {
-        REVERT_LIST(y->sibling);
-        (y->sibling)->sibling = y;
-    } else 
-    {
-        Hr = y;
+
+// Fix the Red-Black Tree properties after deletion
+void deleteFixup(struct Node **root, struct Node *x) {
+    while (x != NULL && x != *root && x->color == 'B') {
+        if (x == x->parent->left) {
+            struct Node *w = x->parent->right;
+           
+            if (w != NULL && w->color == 'R') {
+                w->color = 'B';
+                x->parent->color = 'R';
+                leftRotate(root, x->parent);
+                w = x->parent->right;
+            }
+
+            if (w != NULL && w->left != NULL && w->left->color == 'B' && w->right != NULL && w->right->color == 'B') {
+                w->color = 'R';
+                x = x->parent;
+            } else {
+                if (w != NULL && w->right != NULL && w->right->color == 'B') {
+                    if (w->left != NULL)
+                        w->left->color = 'B';
+                    w->color = 'R';
+                    rightRotate(root, w);
+                    w = x->parent->right;
+                }
+
+                if (w != NULL)
+                    w->color = x->parent->color;
+                x->parent->color = 'B';
+
+                if (w != NULL && w->right != NULL)
+                    w->right->color = 'B';
+
+                leftRotate(root, x->parent);
+                x = *root;
+            }
+        } else {
+            struct Node *w = x->parent->left;
+           
+            if (w != NULL && w->color == 'R') {
+                w->color = 'B';
+                x->parent->color = 'R';
+                rightRotate(root, x->parent);
+                w = x->parent->left;
+            }
+
+            if (w != NULL && w->right != NULL && w->right->color == 'B' && w->left != NULL && w->left->color == 'B') {
+                w->color = 'R';
+                x = x->parent;
+            } else {
+                if (w != NULL && w->left != NULL && w->left->color == 'B') {
+                    if (w->right != NULL)
+                        w->right->color = 'B';
+                    w->color = 'R';
+                    leftRotate(root, w);
+                    w = x->parent->left;
+                }
+
+                if (w != NULL)
+                    w->color = x->parent->color;
+                x->parent->color = 'B';
+
+                if (w != NULL && w->left != NULL)
+                    w->left->color = 'B';
+
+                rightRotate(root, x->parent);
+                x = *root;
+            }
+        }
     }
+
+    if (x != NULL)
+        x->color = 'B';
 }
 
-struct node* FIND_NODE(struct node* H, int k) 
-{
-    struct node* x = H;
-    struct node* p = NULL;
-        if (x->n == k) 
-        {
-        p = x;
-        return p;
+// Delete a node from the Red-Black Tree
+void deleteNode(struct Node **root, int data) {
+    struct Node *z = *root;
+    while (z != NULL) {
+        if (data < z->data)
+            z = z->left;
+        else if (data > z->data)
+            z = z->right;
+        else
+            break; // Node with data found
     }
-    if (x->child != NULL && p == NULL) 
-    {
-        p = FIND_NODE(x->child, k);
+
+    if (z == NULL) {
+        printf("Data %d not found in the tree.\n", data);
+        return;
     }
- 
-    if (x->sibling != NULL && p == NULL) 
-    {
-        p = FIND_NODE(x->sibling, k);
+
+    struct Node *y = z;
+    char yOriginalColor = y->color;
+    struct Node *x;
+
+    if (z->left == NULL) {
+        x = z->right;
+        transplant(root, z, z->right);
+    } else if (z->right == NULL) {
+        x = z->left;
+        transplant(root, z, z->left);
+    } else {
+        y = treeMinimum(z->right);
+        yOriginalColor = y->color;
+        x = y->right;
+
+        if (y->parent == z)
+            x->parent = y;
+        else {
+            transplant(root, y, y->right);
+            y->right = z->right;
+            y->right->parent = y;
+        }
+
+        transplant(root, z, y);
+        y->left = z->left;
+        y->left->parent = y;
+        y->color = z->color;
     }
-    return p;
-}
- 
-int bin_HEAP_DECREASE_KEY(struct node* H, int i, int k) 
-{
-    int temp;
-    struct node* p;
-    struct node* y;
-    struct node* z;
-    p = FIND_NODE(H, i);
-    if (p == NULL) 
-    {
-        printf("\nINVALID CHOICE OF KEY TO BE REDUCED");
-        return 0;
-    }
-    if (k > p->n) 
-    {
-        printf("\nSORY!THE NEW KEY IS GREATER THAN CURRENT ONE");
-        return 0;
-    }
-    p->n = k;
-    y = p;
-    z = p->parent;
-    while (z != NULL && y->n < z->n) 
-    {
-        temp = y->n;
-        y->n = z->n;
-        z->n = temp;
-        y = z;
-        z = z->parent;
-    }
-    printf("\nKEY REDUCED SUCCESSFULLY!");
+
+    free(z);
+
+    if (yOriginalColor == 'B')
+        deleteFixup(root, x);
 }
 
-int bin_HEAP_DELETE(struct node* H, int k) 
-{
-    struct node* np;
-    if (H == NULL) 
-    {
-        printf("\nHEAP EMPTY");
-        return 0;
+// Perform in-order traversal of the Red-Black Tree
+void inOrderTraversal(struct Node *root) {
+    if (root != NULL) {
+        inOrderTraversal(root->left);
+        printf("%d%c ", root->data, root->color);
+        inOrderTraversal(root->right);
     }
- 
-    bin_HEAP_DECREASE_KEY(H, k, -1000);
-    np = bin_HEAP_EXTRACT_MIN(H);
-    if (np != NULL)
-        printf("\nNODE DELETED SUCCESSFULLY");
-}
- 
-int main() 
-{
-    int i, n, m, l;
-    struct node* p;
-    struct node* np;
-    char ch;
-    printf("\nENTER THE NUMBER OF ELEMENTS:");
-    scanf("%d", &n);
-    printf("\nENTER THE ELEMENTS:\n");
-    for (i = 1; i <= n; i++) 
-    {
-        scanf("%d", &m);
-        np = CREATE_NODE(m);
-        H = bin_HEAP_INSERT(H, np);
-    }
-    DISPLAY(H);
-    do 
-    {
-        printf("\nMENU:-\n");
-        printf(
-                "\n1)INSERT AN ELEMENT\n2)EXTRACT THE MINIMUM KEY NODE\n3)DECREASE A NODE KEY\n4)DELETE A NODE\n5)QUIT\n");
-        scanf("%d", &l);
-        switch (l) 
-        {
-        case 1:
-            do 
-            {
-                printf("\nENTER THE ELEMENT TO BE INSERTED:");
-                scanf("%d", &m);
-                p = CREATE_NODE(m);
-                H = bin_HEAP_INSERT(H, p);
-                printf("\nNOW THE HEAP IS:\n");
-                DISPLAY(H);
-                printf("\nINSERT MORE(y/Y)= \n");
-                fflush(stdin);
-                scanf("%c", &ch);
-            } while (ch == 'Y' || ch == 'y');
-            break;
-        case 2:
-            do 
-            {
-                printf("\nEXTRACTING THE MINIMUM KEY NODE");
-                p = bin_HEAP_EXTRACT_MIN(H);
-                if (p != NULL)
-                    printf("\nTHE EXTRACTED NODE IS %d", p->n);
-                printf("\nNOW THE HEAP IS:\n");
-                DISPLAY(H);
-                printf("\nEXTRACT MORE(y/Y)\n");
-                fflush(stdin);
-                scanf("%c", &ch);
-            } while (ch == 'Y' || ch == 'y');
-            break;
-        case 3:
-            do 
-            {
-                printf("\nENTER THE KEY OF THE NODE TO BE DECREASED:");
-                scanf("%d", &m);
-                printf("\nENTER THE NEW KEY : ");
-                scanf("%d", &l);
-                bin_HEAP_DECREASE_KEY(H, m, l);
-                printf("\nNOW THE HEAP IS:\n");
-                DISPLAY(H);
-                printf("\nDECREASE MORE(y/Y)\n");
-                fflush(stdin);
-                scanf("%c", &ch);
-            } while (ch == 'Y' || ch == 'y');
-            break;
-        case 4:
-            do 
-            {
-                printf("\nENTER THE KEY TO BE DELETED: ");
-                scanf("%d", &m);
-                bin_HEAP_DELETE(H, m);
-                printf("\nDELETE MORE(y/Y)\n");
-                fflush(stdin);
-                scanf("%c", &ch);
-            } while (ch == 'y' || ch == 'Y');
-            break;
-        case 5:
-            printf("\nExiting....\n");
-            break;
-        default:
-            printf("\nINVALID ENTRY...TRY AGAIN....\n");
-	}
-    } while (l != 5);
 }
 
